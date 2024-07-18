b36a82d37c59a3eada4adc3195aeac3b9146b75d
jinchenxing <2541226493@qq.com>
2024-07-18 15:34:30 +0800

    fix:07/18

---
+++ dsh-client-api/src/main/java/com/nh/dsh/client/model/dto/FeedbackDTO.java
@@ -0,0 +1,10 @@
+package com.nh.dsh.client.model.dto;
+/**
+* @program : dsh-client-api
+* @description : 
+* @author : Flobby
+* @create : 2024-07-18 14:32
+**/
+ 
+public class FeedbackDTO {
+}
---
+++ dsh-client-api/src/main/java/com/nh/dsh/client/service/FeedbackService.java
@@ -0,0 +1,10 @@
+package com.nh.dsh.client.service;
+/**
+* @program : dsh-client-api
+* @description : 
+* @author : Flobby
+* @create : 2024-07-18 14:34
+**/
+ 
+public interface FeedbackService {
+}
